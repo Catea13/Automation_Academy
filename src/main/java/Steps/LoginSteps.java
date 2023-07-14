39379a8716746a77b5f9e1f2469bbad1e4db6749
ecaterina.hilea <ecaterina.hilea@maib.com>
2023-07-14 16:12:31 +0300

    Add test data;

--- src/main/java/Steps/LoginSteps.java
+++ src/main/java/Steps/LoginSteps.java
@@ -2,6 +2,7 @@ package Steps;
 
 
 import Hook.TestContext;
+import Pages.CheckOutPage;
 import Pages.LoginPage;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.When;
@@ -18,12 +19,23 @@ public class LoginSteps {
     @When("fill in {string} with {string}")
     public void fillInWith(String field, String value) {
         LoginPage loginPage = PageFactory.initElements(testContext.getHook().driver, LoginPage.class);
+
+        CheckOutPage checkOutPage = PageFactory.initElements(testContext.getHook().driver, CheckOutPage.class);
         switch (field) {
             case "Name":
                 loginPage.usernameField.sendKeys(value);
                 break;
             case "Password":
                 loginPage.passwordField.sendKeys(value);
+                break;
+            case "firstNameCheckoutField":
+                checkOutPage.firstNameCheckoutField.sendKeys(value);
+                break;
+            case "lastNameCheckoutField":
+                checkOutPage.lastNameCheckoutField.sendKeys(value);
+                break;
+            case "Zip/Postal Code":
+                checkOutPage.postalCodeCheckoutField.sendKeys(value);
                 break;
 //            default:
 //                System.out.println("This field" + field + " " + "not existed");
@@ -34,9 +46,13 @@ public class LoginSteps {
     @And("click  {string} button")
     public void clickButton(String button) {
         LoginPage loginPage = PageFactory.initElements(testContext.getHook().driver, LoginPage.class);
+        CheckOutPage checkOutPage=PageFactory.initElements(testContext.getHook().driver,CheckOutPage.class);
         switch (button) {
             case "Login":
                 loginPage.loginButton.click();
+                break;
+            case "Continue":
+                checkOutPage.continueButton.click();
                 break;
             default:
                 System.out.println("This button" + button+" "+ "not existed");
--- src/main/java/Tests/CheckOut.feature
+++ src/main/java/Tests/CheckOut.feature
@@ -1,20 +1,18 @@
 Feature: CheckOut
 
-  Scenario : Check that the checkout is possible
-    When    fill in "Name" with "standard_user"
-    And  fill in "Password" with "secret_sauce"
-    And  click  "Login" button
-    And click the button "Add to cart"
-    And click the button "ShoppingCartContainer"
-    And click the button "Checkout"
-    And fill in "First Name" with "Ecaterina"
-    And fill in "Last Name" with "H"
-    And fill in "Zip/Postal Code" with "1234"
-    And the user fills out the questionnaire Checkout with following data
-     Example:
-      | field           | value     |
-      | First Name      | Ecaterina |
-      | Last Name       | H         |
-      | Zip/Postal Code | 1234      |
-    And click the button "Continue"
+  Scenario Outline: Check that the checkout is possible
+    When fill in <field> with <value>
+    And fill in <field1> with <value1>
+    And click  <button> button
+    And click the button <button1>
+    And click the button <button2>
+    And click the button <button3>
+    And fill in <field2> with <value2>
+    And fill in <field3> with <value3>
+    And fill in <field4> with <value4>
+
+    And click the button <button4>
+    Examples:
+      | field  | value           | field1     | value1         | button  | button1       | button2                 | button3    | field2       | value2      | field3      | value3 | field4            | value4 | button4    |
+      | "Name" | "standard_user" | "Password" | "secret_sauce" | "Login" | "Add to cart" | "ShoppingCartContainer" | "Checkout" | "firstNameCheckoutField" | "Ecaterina" | "lastNameCheckoutField" | "H"    | "Zip/Postal Code" | "1234" | "Continue" |
 
